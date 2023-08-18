d53ffce0ae4f992e7772795c482a26704d8f14ee
joaovictorr07 <joaovictor.fernandespereira@gmail.com>
2023-08-17 21:23:33 -0300

    Ajustes server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const app = require("./config/express"); // Importa o app do arquivo ./config/express
 const PORT = process.env.PORT || 3030;
 
-http.createServer(app).listen(3000, function() {
-  console.log(`server started on port ${PORT}`);
+http.createServer(app).listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
