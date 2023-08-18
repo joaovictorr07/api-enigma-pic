dabfa57c4eb0017ea7b0473948869a33b1eb1906
joaovictorr07 <joaovictor.fernandespereira@gmail.com>
2023-08-17 21:14:14 -0300

    Ajustes server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
+http.createServer(app).listen(3000, function() {
   console.log(`server started on port ${PORT}`);
 });
