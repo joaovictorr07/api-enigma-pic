7fe605b5973c2f04913558ed8c954e94d898375f
joaovictorr07 <joaovictor.fernandespereira@gmail.com>
2023-08-17 21:41:34 -0300

    Ajustes erros server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const app = require("./config/express");
+const app = require("./config/express"); // Importa o app do arquivo ./config/express
 const PORT = process.env.PORT || 3030;
 
-http.createServer(app).listen(PORT, function () {
+http.createServer(app).listen(PORT, function() {
   console.log(`Server started on port ${PORT}`);
 });
